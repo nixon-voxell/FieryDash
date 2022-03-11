c530f87086508c76743e2b51237e1a6f2fb73e21
Nixon <voxell.technologies@gmail.com>
2022-03-11 17:13:23 +0800

    player 'fiery' vfx

--- Assets/Scripts/Player/PlayerMovement.Collision.cs
+++ Assets/Scripts/Player/PlayerMovement.Collision.cs
@@ -7,30 +7,32 @@ public partial class PlayerMovement
   {
     _down_boxCastOrigin = position - new float2(0.0f, _halfHeight);
     _down_raycastHit = Physics2D.BoxCast(
-      _down_boxCastOrigin, _down_boxCastSize, 0.0f, Vector2.down, 5.0f
+      _down_boxCastOrigin, _down_boxCastSize, 0.0f, Vector2.down, 5.0f, _solidLayer
     );
 
     Collider2D collider = _down_raycastHit.collider;
     _groundDetected = collider != null;
     _isGrounded = _down_raycastHit.distance < _contactOffset && _groundDetected;
-
-    if (_isGrounded) _dead = CollisionIsLayer(collider.gameObject.layer, in _killableLayer);
   }
 
   private void ObstacleCheck(in float2 position)
   {
     _right_boxCastOrigin = position + new float2(_halfHeight, 0.0f);
     _right_raycastHit = Physics2D.BoxCast(
-      _right_boxCastOrigin, _right_boxCastSize, 0.0f, Vector2.right, 5.0f
+      _right_boxCastOrigin, _right_boxCastSize, 0.0f, Vector2.right, 5.0f, _solidLayer
     );
 
     Collider2D collider = _right_raycastHit.collider;
     _obstacleDetected = collider != null;
     _isObstructed = _right_raycastHit.distance < _contactOffset && _obstacleDetected;
-
-    if (_isObstructed) _dead = CollisionIsLayer(collider.gameObject.layer, in _killableLayer);
   }
 
   private bool CollisionIsLayer(in int collisionLayer, in LayerMask layerMask)
     => (1 << collisionLayer & layerMask) != 0;
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (CollisionIsLayer(collision.gameObject.layer, _killableLayer))
+      _dead = true;
+  }
 }
