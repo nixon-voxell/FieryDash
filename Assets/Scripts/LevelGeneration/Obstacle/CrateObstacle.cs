07648cd51353321f203c7b9511aab5539713bdf1
Goo Wei Lun <gooweilun@gmail.com>
2022-03-12 17:54:16 +0800

    Added interface for determine chance of spawning powerups on crate prefab

--- Assets/Scripts/LevelGeneration/Obstacle/CrateObstacle.cs
+++ Assets/Scripts/LevelGeneration/Obstacle/CrateObstacle.cs
@@ -1,7 +1,33 @@
 using UnityEngine;
+using System.Collections;
+using Random = System.Random;
 
 public class CrateObstacle : AbstractObstacle
 {
+  [Header("PowerUp Config")]
+  [SerializeField, Range(1, 5)] private int _spawnPowerUpChance;
+  Random random = new Random();
+  
+  public void DecidePowerUpSpawn()
+  {
+    int spawn = -1;
+    float[] _spawnPowerUpChanceList = {0.2f,0.4f,0.6f,0.8f,1.0f};
+    double randomDouble = random.NextDouble();
+    float randomFloat = (float) randomDouble;
+    if (randomFloat < _spawnPowerUpChanceList[_spawnPowerUpChance-1])
+    {
+      spawn = 1;
+    }
+    else
+    {
+      spawn = -1;
+    }
+    if (spawn == 1)
+    {
+      PowerUps.SpawnPowerUps();
+    }
+  }
+ 
   public override void Spawn(ref Platform platform, int index, float height)
   {
     base.Spawn(ref platform, index, height);
