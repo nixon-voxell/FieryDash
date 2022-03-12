f61b5803a52f43a744dced33d1f78413050565c8
Nixon <voxell.technologies@gmail.com>
2022-03-12 22:06:10 +0800

    fixed lean tween bug

--- Assets/Scripts/SettingsAnimation.cs
+++ Assets/Scripts/SettingsAnimation.cs
@@ -1,21 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SettingsAnimation : MonoBehaviour
 {
-    private void Start()
-    {
-        transform.localScale = Vector2.zero;
-    }
-    public void Open() 
-    {
-        transform.LeanScale(Vector2.one, 0.3f);
-        Debug.Log("open");
-    }
-    public void Close()
-    {
-        transform.LeanScale(Vector2.zero, 0.5f).setEaseInBack();
-        Debug.Log("Close");
-    }
+  private void Awake() => LeanTween.reset();
+
+  private void Start()
+  {
+    transform.localScale = Vector2.zero;
+  }
+
+  public void Open() 
+  {
+    transform.LeanScale(Vector2.one, 0.3f);
+  }
+  public void Close()
+  {
+    transform.LeanScale(Vector2.zero, 0.5f).setEaseInBack();
+  }
 }
